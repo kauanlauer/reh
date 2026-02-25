c8ec6387d81296d90e008f6d2c090ce4e915d285
Kauan Lauer <kauanlauer@gmail.com>
2026-02-25 20:11:35 -0300

    novo

---
+++ firebase_config.js
@@ -0,0 +1,15 @@
+// --- ARQUIVO DE BACKUP DAS CHAVES ---
+// Atualmente, o sistema "App Like" usa essas chaves diretamente
+// dentro dos arquivos .html para facilitar o uso sem servidor.
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCM9tMvnRV-o7X66euCBKuLmuz-kIrClWY",
+    authDomain: "renata-26079.firebaseapp.com",
+    projectId: "renata-26079",
+    storageBucket: "renata-26079.firebasestorage.app",
+    messagingSenderId: "995787482442",
+    appId: "1:995787482442:web:fdbca404a0e251cc278db3"
+};
+
+// Se um dia quiser centralizar, você usaria assim:
+// export { firebaseConfig };
